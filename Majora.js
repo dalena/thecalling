9035f81ed041e65e9c11994945afb2c9b25fe4d6
hsab <hsab@users.noreply.github.com>
2018-03-14 04:08:44 -0600

    sound added

--- Majora.js
+++ Majora.js
@@ -1,93 +1,180 @@
 "use strict"
 
+var sourceRepo = "https://github.com/dalena/thecalling";
+var redirect = false;
+var redir_seconds = 10;
+var sound_count = 1
+
+var sounds = {
+    "bg": function () {
+        var sound = new Howl({
+            src: [
+                './assets/audio/bg_theme.altconv.webm',
+                './assets/audio/bg_theme.altconv.oga',
+                './assets/audio/bg_theme.mp3'
+            ],
+            autoplay: false,
+            rate: 0.3,
+            loop: true,
+            volume: 0
+        });
+
+        sound.setEvent = function (event, callback) {
+            sound.once(event, callback)
+        }
+
+        sounds.bg = sound;
+    },
+
+    "intro": function () {
+        var sound = new Howl({
+            src: [
+                './assets/audio/mar_1.altconv.webm',
+                './assets/audio/mar_1.altconv.oga',
+                './assets/audio/mar_1.mp3'
+            ],
+            autoplay: false,
+            rate: 0.8,
+            volume: 0
+        });
+
+        sounds.intro = sound;
+    },
+
+    "outro": function () {
+        var idx = Math.floor(Math.random() * sound_count) + 1;
+        var sound = new Howl({
+            src: [
+                './assets/audio/mar_' + idx + '.altconv.webm',
+                './assets/audio/mar_' + idx + '.altconv.oga',
+                './assets/audio/mar_' + idx + '.mp3'
+            ],
+            autoplay: false,
+            volume: 0
+        });
+
+        sounds.outro = sound;
+    },
+
+    "load": function (key, from, to, dur, callback) {
+        sounds[key]();
+        sounds[key].play();
+        sounds[key].fade(from, to, dur);
+    }
+}
+
 // register the application module
-b4w.register("Majora_main", function(exports, require) {
+b4w.register("Majora_main", function (exports, require) {
 
-// import modules used by the app
-var m_app       = require("app");
-var m_cfg       = require("config");
-var m_data      = require("data");
-var m_preloader = require("preloader");
-var m_ver       = require("version");
+    // MODULES
+    var m_app = require("app");
+    var m_main = require("main");
+    var m_cfg = require("config");
+    var m_data = require("data");
+    var m_preloader = require("preloader");
+    var m_ver = require("version");
 
-// detect application mode
-var DEBUG = (m_ver.type() == "DEBUG");
+    // EXPORTS
+    exports.init = init;
+    exports.webgl_failed = webgl_failed
 
-// automatically detect assets path
-var APP_ASSETS_PATH = m_cfg.get_assets_path("Majora");
+    // APP MODE
+    var DEBUG = (m_ver.type() == "DEBUG");
 
-/**
- * export the method to initialize the app (called at the bottom of this file)
- */
-exports.init = function() {
-    m_app.init({
-        canvas_container_id: "main_canvas_container",
-        callback: init_cb,
-        show_fps: DEBUG,
-        console_verbose: DEBUG,
-        autoresize: true
-        // assets_gzip_available: true
-    });
-}
+    // DETECT ASSETS PATH
+    var APP_ASSETS_PATH = m_cfg.get_assets_path("Majora");
 
-/**
- * callback executed when the app is initialized 
- */
+    function init() {
+        console.log("Source available at:", sourceRepo);
 
-function init_cb(canvas_elem, success) {
-
-    if (!success) {
-        console.log("b4w init failure");
-        return;
+        if (true)
+            m_app.init({
+                canvas_container_id: "main_canvas_container",
+                callback: init_cb,
+                show_fps: DEBUG,
+                console_verbose: DEBUG,
+                autoresize: true,
+                report_init_failure: false
+                // assets_gzip_available: true
+            });
     }
 
-    document.getElementById("preloader_cont").style.visibility = "visible"
-    // m_preloader.create_preloader();
+    function init_cb(canvas_elem, success) {
 
-    // ignore right-click on the canvas element
-    canvas_elem.oncontextmenu = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-    };
+        if (!success) {
+            webgl_failed();
+            return;
+        }
 
-    load();
-}
+        // Start BACKGROUND theme sound
+        sounds.load("bg", 0, 0.1, 6000);
 
-/**
- * load the scene data
- */
-function load() {
-    m_data.load(APP_ASSETS_PATH + "Majora.json", load_cb, preloader_cb);
-}
 
-/**
- * update the app's preloader
- */
-function preloader_cb(percentage) {
-    document.getElementById("prelod_dynamic_path").style.width = percentage + "%";
-    100 == percentage && (document.getElementById("preloader_cont").style.visibility = "hidden")
-    // m_preloader.update_preloader(percentage);
-}
+        $('#preloader_cont').css('visibility', 'visible');
+        $('#preloader_cont').removeClass('opacity-zero');
+        $('#preloader_cont').addClass('opacity-full');
 
-/**
- * callback executed when the scene data is loaded
- */
-function load_cb(data_id, success) {
+        // ignore right-click on the canvas element
+        canvas_elem.oncontextmenu = function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        };
 
-    if (!success) {
-        console.log("b4w load failure");
-        return;
+        load();
     }
 
-    m_app.enable_camera_controls();
+    function load() {
+        m_data.load(APP_ASSETS_PATH + "Majora.json", load_cb, preloader_cb);
+    }
 
-    // place your code here
+    function preloader_cb(percentage) {
+        $('#prelod_dynamic_path').css('width', percentage + "%");
+        if (percentage == 100) {
+            $('#preloader_cont').removeClass('opacity-full');
+            $('#preloader_cont').addClass('opacity-zero');
+            setTimeout(function () {
+                $('#preloader_cont').remove();
+                $('#webgl-fail').remove();
+            }, 500)
+        }
+    }
 
-}
+    function load_cb(data_id, success) {
 
+        if (!success) {
+            console.log("Loading failed.");
+            return;
+        }
+
+        m_app.enable_camera_controls();
+
+        // If success for load, play the INTRO sound
+        setTimeout(function () {
+            sounds.load("intro", 0, 0.5, 1000);
+        }, 6000);
+    }
+
+    function webgl_failed() {
+        console.log("WebGL initialization failed.");
+
+        $('#main_canvas_container').remove();
+        $('#webgl-fail').removeClass('opacity-zero');
+        $('#webgl-fail').addClass('opacity-full');
+
+
+        var seconds_span = $('#redir-seconds');
+
+        function incrementSeconds() {
+            redir_seconds -= 1;
+            seconds_span.text(redir_seconds)
+            redir_seconds == 0 && redirect && (document.location.href = "http://sevdaliza.com");
+        }
+
+        var cancel = setInterval(incrementSeconds, 1000);
+    }
 
 });
 
-// import the app module and start the app by calling the init method
-b4w.require("Majora_main").init();
+var majora = b4w.require("Majora_main");
+majora.init();
