dd7ff3eac0a0c08646a0ad30ae7bf40a33262478
hsab <hsab@users.noreply.github.com>
2018-03-18 21:44:27 -0600

    cleanup

--- Main/js/sketch.js
+++ Main/js/sketch.js
@@ -1,3 +1,9 @@
+// "Ripples" by Bruce Luo
+// http://www.openprocessing.org/sketch/446986
+// Licensed under Creative Commons Attribution ShareAlike
+// https://creativecommons.org/licenses/by-sa/3.0
+// https://creativecommons.org/licenses/GPL/2.0/
+
 const block_size = 25;
 const block_core = 1;
 const block_move_distance = 10;
