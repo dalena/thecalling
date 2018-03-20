55acd7ce8541f45dea4b531ff1462813824515e8
hsab <hsab@users.noreply.github.com>
2018-03-20 11:06:41 -0600

    mobile + parallax

--- Main/js/main.js
+++ Main/js/main.js
@@ -3,6 +3,12 @@ var controller = new ScrollMagic.Controller();
 var sceneTitle;
 var sceneCover;
 
+var scene = document.getElementById('parallax');
+var parallaxInstance = new Parallax(scene);
+parallaxInstance.friction(0.3, 0.1);
+parallaxInstance.invert(true, true);
+
+
 $(window).on('resize', function () {
     sceneTitle.destroy(true);
     // sceneCover.destroy(true);
@@ -12,14 +18,14 @@ $(window).on('resize', function () {
 
 
 function initSceneTitle() {
-    // var wipeAnimation = new TimelineMax()
-    //     .fromTo(".cover-img", 1, {
-    //         opacity: 0,
-    //         width: "0%"
-    //     }, {
-    //         opacity: 1,
-    //         width: "70%"
-    //     }, '0')
+    var wipeAnimation = new TweenMax
+        .fromTo(".cover-img", 1, {
+            opacity: 0,
+            width: "0%"
+        }, {
+            opacity: 1,
+            width: "70%"
+        }, '0')
 
 
     sceneTitle = new ScrollMagic.Scene({
@@ -31,7 +37,7 @@ function initSceneTitle() {
             pushFollowers: false
             // y: "-100%",
         })
-        // .setTween(wipeAnimation)
+        .setTween(wipeAnimation)
         .on("enter", function (e) {
             $("#pin1").css("z-index", 9999);
             document.getElementById("title-vid").play();
